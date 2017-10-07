2da0e53c9dc172e221f3a44d0b9180bb73ad30e6
Jorge Sacadura <hardbag@Jorges-MBP.lan>
2017-10-07 18:56:40 +0100

    client is sending commands for snake

--- project/client.py
+++ project/client.py
@@ -1,11 +1,25 @@
 import socket
+import curses
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 
 s = socket.socket()
 host = socket.gethostbyname('localhost')
-port = 12345
+port = 12340
 s.connect((host, port))
 
+key = KEY_RIGHT
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)
 while True:
-    request = raw_input("> ")
-    s.send(request)
-    print s.recv(1024)
+    event = win.getch()
+    key = key if event == -1 else event
+    if key == KEY_RIGHT:
+        s.send(str(KEY_RIGHT))
+    elif key == KEY_LEFT:
+        s.send(str(KEY_LEFT))
+    elif key == KEY_UP:
+        s.send(str(KEY_UP))
+    elif key == KEY_DOWN:
+        s.send(str(KEY_DOWN))
+    elif key == 27:
+        s.send(str(27))
